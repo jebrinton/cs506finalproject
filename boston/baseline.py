4ffd7f84fa8a41657853749ef9a7968eb1c09108
Jacob Brinton <jbrin@bu.edu>
2025-03-30 21:57:22 -0400

    Minor changes (merging cells, removing redundant code) to combined notebook and added baseline (i.e. predictions of tomorrow's weather using today's)

--- boston/baseline.py
+++ boston/baseline.py
@@ -3,11 +3,22 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("boston_weather_data.csv")
+# Storing the dataset into the variable dataset and making the "DATE" column the index column.
+dataset = pd.read_csv("weather.csv", index_col="DATE")
 
-# Use 'time' as index
-df['time'] = pd.to_datetime(df['time'])
-df.set_index('time', inplace=True)
+# For each column, store the percentage of missing values.
+miss_pct = dataset.apply(pd.isnull).sum()/dataset.shape[0]
+
+# List columns with low miss_pct (good columns); feel free to tweak the threshold of how low we want missing values to be.
+good_columns = dataset.columns[miss_pct < 0.01]
+dataset = dataset[good_columns].copy()
+dataset.apply(pd.isnull).sum()/dataset.shape[0]
+
+# Since the "PRCP" column has missin values, we fill the missing values with zero since
+# if there was no PRCP recorded for that day then that must mean that it did not rain therefore PRCP would be zero
+dataset["PRCP"] = dataset["PRCP"].fillna(0)
+
+dataset.apply(pd.isnull).sum() / dataset.shape[0]
 
 # Fill in NaN
 df = df.interpolate(method='linear')
@@ -15,6 +26,6 @@ df = df.interpolate(method='linear')
 # get the average difference in max tempurature between days
 print(df)
 # %%
-diff = np.diff(df['tmax'].values)
+diff = np.diff(df['TMAX'].values)
 
 print("MAE if predicted tomorrow's max with today's max: ", diff.sum().round(4))
